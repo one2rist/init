c6aa9083e4ec1d3b0dc0ff47ba404667c9b52055
Igor Bakulin <igorbakulin@gmail.com>
2023-11-11 21:49:11 +0200

    add-clang-format-script

--- init/backup_vim.sh
+++ init/backup_vim.sh
@@ -1,8 +1,6 @@
 cp ~/.gitconfig         ~/init/home/_gitconfig
 cp ~/.gitignore         ~/init/home/_gitignore
-cp ~/.vimrc             ~/init/home/_vimrc    
-#cp ~/.clang-format      ~/init/home/_clang-format
-
+cp ~/.vimrc             ~/init/home/_vimrc
 
 cd ~/init
 git diff
---
+++ scripts/clang-format.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo >&2 "Running pre-commit hook"
+srcpattern='.*\.(c|cc|cpp|cxx|h|hh|hpp|hxx)$'
+cmkpattern='(CMakeLists\.txt|*\.cmake)$'
+excpattern='(?!3rdParty|build/)'
+root=$(git rev-parse --show-toplevel)
+#scope="git diff-index --cached --name-only HEAD"
+scope="find . -type f"
+cd ${root}
+for file in `${scope} | grep -vE ${excpattern} | grep -iE ${srcpattern}` ; do
+    f=${root}/${file}
+    clang-format -i ${f}
+    echo >&2 "CLANG-FORMAT: ${f}"
+    git add ${f}
+done
+for file in `${scope} | grep -vE ${excpattern} | grep -iE ${cmkpattern}` ; do
+    f=${root}/${file}
+    cmake-format -i ${f}
+    echo >&2 "CMAKE-FORMAT: ${f}"
+    git add ${f}
+done
