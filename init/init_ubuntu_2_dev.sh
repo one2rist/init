9b3d9b67ed05396d044b56fca5b18f107d549650
Igor Bakulin <igorbakulin@gmail.com>
2023-11-14 20:21:14 +0200

    split-script-init-ubuntu

--- init/init_ubuntu_2_dev.sh
+++ init/init_ubuntu_2_dev.sh
@@ -1,12 +1,4 @@
 echo ""
-echo "installing unity hub"
-echo ""
-sudo sh -c 'echo "deb https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
-wget -qO - https://hub.unity3d.com/linux/keys/public | sudo apt-key add -
-sudo apt update
-sudo apt-get -y install unityhub
-
-echo ""
 echo "installing dev tools"
 echo ""
 sudo apt -y install curl
